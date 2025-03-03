0b2ac4c8bf47c36b24f4217703568b15e90c3c44
parthpansuriya0 <pansuriyaparth2003@gmail.com>
2025-03-03 14:30:00 +0530

    first commit

---
+++ datafetch.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+# import requests
+from urllib.request import urlopen
+import json
+
+url = "https://books.toscrape.com/"
+# r = requests.get(url)
+page = urlopen(url)
+soup = BeautifulSoup(page,'html.parser')
+setdata = []
+getdata = soup.find_all('article',class_='product_pod')
+
+for d in getdata :
+    name = d.find('h3').text.strip()
+    price = d.find('p',class_='price_color').text.strip()
+    stock = d.find('p',class_='instock').text.strip()
+
+    setdata.append({
+        'name':name,
+        'price':price,
+        'stock':stock,
+    })
+
+with open("raw_books_data.json","w") as f:
+    json.dump(setdata,f)
